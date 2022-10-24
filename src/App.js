b5adf3459719700d0134224a74ba44dd8fbd585f
CraigYeoman <yeomancraig@gmail.com>
2022-10-24 17:56:41 -0500

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 
 function App() {
+  
+  const pokemonURL = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151'
+
+  const [pokemon, setPokemon] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [viewPokemon, setViewPokemon] = useState([])
+
+  const fetchPokemon = async(url) => {
+    setLoading(true)
+    try {
+      const {data} = await axios.get(url)
+
+      if(data.results){
+        setPokemon(data.results)
+        console.log(pokemon)
+      } else {
+        setPokemon([])
+      }
+
+    } catch(e){
+      console.log(e)
+    }
+    setLoading(false)
+  }
+
+  const fetchPokemonData = async(url) => {
+    setLoading(true)
+    try {
+      const {data} = await axios.get(url)
+
+      if(data.sprites){
+        setViewPokemon(data)
+        console.log(viewPokemon)
+      } else {
+        setViewPokemon([])
+      }
+
+    } catch(e){
+      console.log(e)
+    }
+    setLoading(false)
+  }
+
+  useEffect(()=>{
+    fetchPokemon(pokemonURL)
+    console.log(pokemon)
+  }, [])
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <div>{pokemon.map((unit, index)=> {
+        return (
+          <div key={index}>
+            <div>{unit.name}</div>
+            <div>{index+1}</div>
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png` } alt=''/>
+          </div>
+        )
+      })}</div>
     </div>
   );
 }
